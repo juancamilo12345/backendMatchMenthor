4d2237d7330763bddbb8d7a21570776912b4d4ef
Juan Camilo Medina <juancamilomedinafernandez2020@gmail.com>
2025-10-21 09:54:30 -0500

    se crear archivo backend de matchMenthor

---
+++ matchMenthor/src/main/java/com/matchMenthor/matchMenthor/MatchMenthorApplication.java
@@ -0,0 +1,13 @@
+package com.matchMenthor.matchMenthor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchMenthorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchMenthorApplication.class, args);
+	}
+
+}
---
+++ matchMenthor/src/test/java/com/matchMenthor/matchMenthor/MatchMenthorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchMenthor.matchMenthor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchMenthorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
